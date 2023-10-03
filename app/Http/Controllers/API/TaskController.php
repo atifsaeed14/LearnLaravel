a1b29793af511f29022f0e158446ae489d4e665b
Atif Saeed <atifsaeed14@gmail.com>
2023-10-03 12:54:24 +0500

    php artisan make:resource TaskCollection

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Resources\TaskCollection;
 use App\Models\Task;
 
 class TaskController extends BaseController
 {
     public function index(Request $request)
     {
-        return response()->json(Task::all());
+        // return response()->json(Task::all());
+        return new TaskCollection(Task::all());
     }
 }
